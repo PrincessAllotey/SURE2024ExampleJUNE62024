8f1df087b284502144d23d23af2d4d016a396e8f
Princess Allotey <pallotey@andrew.cmu.edu>
2024-06-07 10:26:09 -0400

    Commit message

--- ALLOTEY/HelloWorld.R
+++ ALLOTEY/HelloWorld.R
@@ -1,2 +1,14 @@
 print("Hello World")
 print("Yes")
+
+library(reshape2)
+library(plotly)
+
+p <- ggplot(tips, aes(x=total_bill, y=tip/total_bill)) + geom_point(shape=1)
+
+# Divide by day, going horizontally and wrapping with 2 columns
+p <- p + facet_wrap( ~ day, ncol=2)
+
+fig <- ggplotly(p)
+
+fig
